4c9a8f7082e6f2e1895e37bd4513db7385798d49
N00budrag0n <helmiagung2468@gmail.com>
2025-04-18 00:12:48 +0700

    update category / preference selection

--- resources/js/Components/SelectButton.jsx
+++ resources/js/Components/SelectButton.jsx
@@ -24,7 +24,7 @@ const SelectButton = ({
                             backgroundColor: selectedValues.includes(
                                 option[valueKey]
                             )
-                                ? "#8F4030"
+                                ? "var(--nav-color)"
                                 : "white",
                             color: selectedValues.includes(option[valueKey])
                                 ? "white"
