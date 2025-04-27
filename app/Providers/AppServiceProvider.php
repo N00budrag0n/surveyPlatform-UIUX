ccab50def9cafd4f91d0da34be8d5eaaf0e4be41
N00budrag0n <helmiagung2468@gmail.com>
2025-04-27 22:38:15 +0700

    add service for text processing service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\TextAnalysisService;
 use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(TextAnalysisService::class, function ($app) {
+            return new TextAnalysisService();
+        });
     }
 
     /**
